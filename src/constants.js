597c761bb1fd77e1ad131ce27a4d2d64649d7c3c
rohanb202 <rohanb22250@gmail.com>
2023-09-26 00:10:39 +0530

    fixes

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
 export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = 'https://e-clone-api.vercel.app/api/products';
+export const USERS_URL = 'https://e-clone-api.vercel.app/api/users';
+export const ORDERS_URL = 'https://e-clone-api.vercel.app/api/orders';
+export const PAYPAL_URL = 'https://e-clone-api.vercel.app/api/config/paypal';
